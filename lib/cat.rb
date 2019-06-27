c0bf3ffe290c1679d2c9cf43068b7c12386b7df7
github username <github email address>
2019-06-27 16:22:25 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Cat
-  # code goes here
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+  # attr_writer
+
+@@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @mood = mood
+    @owner = owner
+    @@all << self
+
+    # binding.pry
+  end
+
+  def owner()
+    @owner
+  end
+
+  def self.all
+    @@all
+  end
+
+# binding.pry
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+require_relative 'owner.rb'
 class Dog
-  # code goes here
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+  # attr_writer
+
+@@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @mood = mood
+    @owner = owner
+    @@all << self
+  end
+
+  def owner()
+    @owner
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
