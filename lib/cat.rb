ba7add555ca67b8cb7539f5a7b221c3afaf966a8
jordanbacon <jmbacon92@gmail.com>
2019-06-27 12:47:25 -0700

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@array = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    #@owner.all << self
+    @@array << self
+  end
+  def self.all
+    @@array
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@array = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@array << self
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+  def mood
+    @mood
+  end
+
+  def self.all
+    @@array
+  end
+
 end
